cfab5115329013d32678cd2517cc87f8c212edae
palinaria <polinapuzik7@gmail.com>
2025-04-03 23:17:16 +0300

    feat:add_tooltip

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,36 +8,29 @@ import Tooltip from "../Tooltip/Tooltip.jsx";
 
 const Main = () => {
     return (<main className="main">
-            <div className="main__title">Browse our menu</div>
-            <div className="main__subtitle common_font">
-                Use our menu to place an order online, or {" "}
-                <Tooltip text="Call us: +37067683921">
+        <div className="main__title">Browse our menu</div>
+        <div className="main__subtitle common_font">
+            Use our menu to place an order online, or {" "}
+            <Tooltip text="Call us: +37067683921">
                 <span className="highlight"> phone</span> </Tooltip> our store <br/> to place a pickup
-                order. Fast and fresh food.
-            </div>
+            order. Fast and fresh food.
+        </div>
 
-            <Menu/>
+        <Menu/>
 
-            <div className="main_menu">
+        <div className="main_menu">
+            {Array.from({length: 6}, (_, index) => (
                 <ProductCard
+                    index={index}
                     title="Burger Dreams"
                     price="9.20"
                     description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
                 />
-                <ProductCard
-                    title="Burger Dreams"
-                    price="9.20"
-                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                />
-                <ProductCard
-                    title="Burger Dreams"
-                    price="9.20"
-                    description="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                />
-            </div>
+            ))}
+        </div>
 
-            <SeeMore/>
-        </main>);
+        <SeeMore/>
+    </main>);
 };
 
 export default Main;
