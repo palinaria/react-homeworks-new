f212854b937b28285ce092f0d84f02473e17b4c0
palinaria <polinapuzik7@gmail.com>
2025-05-09 13:35:49 +0300

    fix:handle API response with mistakes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,6 +19,9 @@ const Main = ({ onAddToCart }) => {
         const fetchItems = async () => {
             try {
                 const response = await fetch("https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/meals");
+                if(!response.ok) {
+                    throw new Error(`Failed to fetch data.Status:${response.status}`);
+                }
                 const result = await response.json();
                 setMenuItems(result);
             } catch (error) {
