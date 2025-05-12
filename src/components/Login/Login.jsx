a20afb878e2b9b37b265d872756f4a96b5ef3715
palinaria <polinapuzik7@gmail.com>
2025-05-12 19:42:36 +0300

    feat:Input is a Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Login.css"
+import Input from "/src/components/Input/Input.jsx"
 
 const Login = () => {
     return (
@@ -8,21 +9,23 @@ const Login = () => {
             <div className="login_content">
                 <div className="user_name">
                     <label>
-                        User name:&nbsp;
-                        <input
+                        User name &nbsp;
+                        <Input
                             type="text"
                             name="username"
                             placeholder="Enter your username"
+                            size="medium"
                         />
                     </label>
                 </div>
                 <div className="password">
                     <label>
-                        Password:&nbsp;
-                        <input
+                        Password &nbsp;
+                        <Input
                             type="password"
                             name="password"
                             placeholder="Enter your password"
+                            size="medium"
                         />
                     </label>
                 </div>
