c91e2d9ebddab987b380fa2ece515f0005974f73
palinaria <polinapuzik7@gmail.com>
2025-06-03 17:46:29 +0300

    fix:auth

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,19 @@ import { onAuthStateChanged ,User} from "firebase/auth";
 
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "./Store/authSlice";
+import { addItem, selectTotalQuantity } from './Store/cartSlice';
 import { RootState, AppDispatch } from "./Store/Store";
 
 import {CartProduct} from "./interfaces/menuItem";
-
-
 
 type CartItems = {
     [productId: string]: CartProduct;
 };
 
 const App:FC = () => {
-    const [cartItems, setCartItems] = useState<CartItems>({});
-    const [totalQuantity, setTotalQuantity] = useState<number>(0);
     const dispatch = useDispatch<AppDispatch>();
     const user = useSelector((state: RootState) => state.auth.user);
+    const totalQuantity = useSelector(selectTotalQuantity); // селектор уже есть
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
@@ -42,31 +40,7 @@ const App:FC = () => {
     }, [dispatch]);
 
     const handleAddToCart = (product: CartProduct) => {
-        const productId = product.id;
-        const productQuantity = product.quantity;
-
-        setCartItems((prevItems) => {
-            const updatedItems = { ...prevItems };
-
-            if (updatedItems[productId]) {
-                updatedItems[productId] = {
-                    ...updatedItems[productId],
-                    quantity: updatedItems[productId].quantity + productQuantity,
-                };
-            } else {
-                updatedItems[productId] = {
-                    id: productId,
-                    meal: product.meal,
-                    price: product.price,
-                    quantity: productQuantity,
-                    instructions: product.instructions,
-                    img: product.img,
-                };
-            }
-            return updatedItems;
-        });
-
-        setTotalQuantity((prevQuantity) => prevQuantity + productQuantity);
+        dispatch(addItem(product));
     };
 
     return (
